14535749c0bbec26d96a8242e191274a7e18dcd9
brianhsieh01 <chyuanxie@gmail.com>
2024-04-25 23:25:18 -0700

    feat: add kafka_enable environment variable

--- main/utils/env_loader.py
+++ main/utils/env_loader.py
@@ -15,18 +15,19 @@ class Default:
     LOGS_FOLDER_PATH:str
     SECRET_KEY:str
     DJANGO_SETTINGS_MODULE:str
-    DEBUG:str
+    DEBUG:bool
     ALLOWED_HOSTS:str
     API_ROOT:str
-    API_VERSION: str
+    API_VERSION:str
     MODEL_SAVE_PATH:str
     MODEL_FILE_NAME:str
     HTTP_KAFKA_HOST:str
     HTTP_KAFKA_PORT1:str
     HTTP_KAFKA_PORT2:str
     HTTP_KAFKA_PORT3:str
-    
-    
+    KAFKA_ENABLE:bool
+
+
 default_env = Default(
     LOGS_FOLDER_PATH=os.environ.get('LOGS_FOLDER_PATH'),
     SECRET_KEY=os.environ.get('SECRET_KEY'),
@@ -40,5 +41,6 @@ default_env = Default(
     HTTP_KAFKA_HOST=os.environ.get('HTTP_KAFKA_HOST'),
     HTTP_KAFKA_PORT1=os.environ.get('HTTP_KAFKA_PORT1'),
     HTTP_KAFKA_PORT2=os.environ.get('HTTP_KAFKA_PORT2'),
-    HTTP_KAFKA_PORT3=os.environ.get('HTTP_KAFKA_PORT3')
+    HTTP_KAFKA_PORT3=os.environ.get('HTTP_KAFKA_PORT3'),
+    KAFKA_ENABLE=os.environ.get('KAFKA_ENABLE')
 )
