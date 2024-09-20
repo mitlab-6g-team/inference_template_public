807a05e86e3f5584c3026526f4b6597ca809d1cf
hpc0326 <jack20010326@gmail.com>
2024-09-20 00:47:23 -0700

    fix: env and params

--- main/apps/inference_exe/services/platform.py
+++ main/apps/inference_exe/services/platform.py
@@ -5,15 +5,13 @@ from main.utils.env_loader import customized_env
 
 def update_position_status(status):
 
-    POSITION_UID = os.environ.get('POSITION_UID'),
-    APPLICATION_UID = os.environ.get('APPLICATION_UID')
+    POSITION_UID = os.environ.get('POSITION_UID')
     DEPLOYMENT_PF_HOST_IP = os.environ.get('DEPLOYMENT_PF_HOST_IP')
 
     payload = {
-        "application_uid": APPLICATION_UID,
         "position_uid": POSITION_UID,
         "status": status
     }
-    url = f"http://{DEPLOYMENT_PF_HOST_IP}/api/{customized_env.DEPLOYMENT_PLATFORM_VERSION}/abstract_metadata/PositionMetadataWriter/update"
+    url = f"http://{DEPLOYMENT_PF_HOST_IP}/api/{customized_env.DEPLOYMENT_PLATFORM_VERSION}/inference_operation/PositionManager/update"
     headers = {'Accept': 'application/json'}
     requests.post(url, json=payload, headers=headers)
